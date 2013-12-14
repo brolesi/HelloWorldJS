0061ace59f766d2729d9c88923b4266238a00271
brolesi <brolesi@gmail.com>
2013-12-14 14:41:58 -0200

    Documentation

---
+++ js/HelloWorld.doc.js
@@ -0,0 +1,18 @@
+/**
+A HelloWorld Class to any-pourpose
+
+@class HelloWorld
+@static
+**/
+var HelloWorld = {
+/**
+Returns the "Hello World" string
+
+@method HelloWorld
+@return "Hello World" string
+**/
+    HelloWorld : function(){
+//    The method return 
+      return "Hello World"
+    }
+};
